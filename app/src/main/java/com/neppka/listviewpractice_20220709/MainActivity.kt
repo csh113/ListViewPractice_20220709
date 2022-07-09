dfe9f56a435ce4090e357daccbaa9247220e5f67
shchoi <gllapia@naver.com>
2022-07-09 14:19:49 +0900

    9. intent 이동 20220709 토

--- app/src/main/java/com/neppka/listviewpractice_20220709/MainActivity.kt
+++ app/src/main/java/com/neppka/listviewpractice_20220709/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.neppka.listviewpractice_20220709
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.neppka.listviewpractice_20220709.adapters.StudentListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.student_list_item.*
 
 class MainActivity : AppCompatActivity() {
 //    ArrayList 사용하기 위해 .. 전역 변수 = 멤버 변수
@@ -37,14 +39,38 @@ class MainActivity : AppCompatActivity() {
         mainListView.adapter = mStudentListAdapter
 
 //        리스트뷰 한줄에 대한 클릭 이벤트 처리 - 오후 수업 시작
-        mainListView.setOnItemClickListener { adapterView, view, position, l ->
-            Toast.makeText(this, "${position}번째 아이템입니다.", Toast.LENGTH_SHORT).show()
-        }
+//        mainListView.setOnItemClickListener { adapterView, view, position, l ->
+//            Toast.makeText(this, "${position}번째 아이템입니다.", Toast.LENGTH_SHORT).show()
+//        }
 
 //        리스트뷰 한줄에 대한 롱클릭 이벤트 처리
         mainListView.setOnItemLongClickListener { adapterView, view, i, l ->
             Toast.makeText(this, "${i}번째 아이템 롱클릭 이벤트 입니다.", Toast.LENGTH_SHORT).show()
             return@setOnItemLongClickListener(true)
         }
+
+//        이벤트 넘기기
+        mainListView.setOnItemClickListener { adapterView, view, position, l ->
+            val myIntent = Intent(this, ProfileActivity::class.java)
+
+            myIntent.putExtra("name", mStudentList[position].name)
+                .putExtra("birthYear", mStudentList[position].birthYear)
+                .putExtra("address", mStudentList[position].address)
+
+            startActivity(myIntent)
+        }
+
+//        아이템 클릭 시 화면 이동
+//        mainListView.setOnClickListener {
+//            val inputName = nameTxt.text.toString()
+//
+//            val myIntent = Intent(this, ProfileActivity::class.java)
+//
+////            이메일이라는 수화물 들고 날라간다.
+////            email이라는 이름표를 만들어서 MainActivity2에 넘긴다.
+//            myIntent.putExtra("inName", inputName)
+////            실제 비행기를 출발
+//            startActivity(myIntent) //myIntent : 비행기 티켓을 들고 타는 느낌
+//        }
     }
 }
--- app/src/main/java/com/neppka/listviewpractice_20220709/ProfileActivity.kt
+++ app/src/main/java/com/neppka/listviewpractice_20220709/ProfileActivity.kt
@@ -2,10 +2,20 @@ package com.neppka.listviewpractice_20220709
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.student_list_item.*
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val name = intent.getStringExtra("name")
+        val birthYear = intent.getIntExtra("birthYear", 0)
+        val address = intent.getStringExtra("address")
+
+//        activity_profile의 것
+        nameTxt.text = name
+        ageTxt.text = birthYear.toString()
+        addressTxt.text = address
     }
 }
