7f810856431c78de580615ddbc151f7d686eff6b
shchoi <gllapia@naver.com>
2022-07-09 13:34:58 +0900

    6. listView 실데이터 연결 20220709 토

--- app/src/main/java/com/neppka/listviewpractice_20220709/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/neppka/listviewpractice_20220709/adapters/StudentListAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppka.listviewpractice_20220709.R
 import com.neppka.listviewpractice_20220709.StudentData
+import org.w3c.dom.Text
 
 class StudentListAdapter(
     val mContext : Context,
@@ -21,6 +23,17 @@ class StudentListAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
         }
         val row = tempRow!!
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+
+//        mList[position] => mList의 position번째 있는 StudentData
+        nameTxt.text = mList[position].name
+//        도전과제 => String을 가공하여 ${} 를 활용해서 => 나이부분의 값이 (~~세)로 표시되도록 가공
+        ageTxt.text = mList[position].birthYear.toString()
+        addressTxt.text = mList[position].address
+
 
         return row
     }
