a4bec06468c2ac47d0f7d2d00a7e202b8fce18b9
shchoi <gllapia@naver.com>
2022-07-09 11:48:00 +0900

    4-2. StudentListAdapter.kt 제작 생성 20220709 토

--- app/src/main/java/com/neppka/listviewpractice_20220709/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/neppka/listviewpractice_20220709/adapters/StudentListAdapter.kt
@@ -1,4 +1,13 @@
 package com.neppka.listviewpractice_20220709.adapters
 
-class StudentListAdapter {
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.neppka.listviewpractice_20220709.StudentData
+
+class StudentListAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<StudentData>
+) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
 }
